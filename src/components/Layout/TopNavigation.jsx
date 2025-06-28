2eac3031c31889ddc156d039cc32840812baab57
premnath045 <40193906+premnath045@users.noreply.github.com>
2025-06-29 00:10:41 +0530

    Updated TopNavigation.jsx

--- src/components/Layout/TopNavigation.jsx
+++ src/components/Layout/TopNavigation.jsx
@@ -44,7 +44,6 @@ function TopNavigation({ onSettingsClick }) {
                       </div>
                     </motion.div>
                   )}
-                  <div className="absolute -top-1 -right-1 w-3 h-3 bg-red-500 rounded-full"></div>
                 </Link>
                 
                 <button 
